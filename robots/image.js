1e62e0af73c8ab8c7edeb9e086e48fe92a435189
Carlos Gimenes <cadugimenes9@gmail.com>
2019-04-30 03:03:30 -0300

    Atualizando arquivos

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imgUrlBlackList = require('./blacklist.json').imageUrlBlackList
 const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
@@ -8,12 +9,12 @@ async function robot() {
     const content = state.load()
 
     //await fetchImagesOfAllSentences(content)
+    //await downloadAllImages(content)
 
-    await downloadAllImages(content)
-    //state.save(content)
+    state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
-        for(const sentence of content.sentences) {
+        for (const sentence of content.sentences) {
             const query = `${content.searchTerm} ${sentence.keywords[0]}`
             sentence.images = await fetchGoogleAndReturnimageLinks(query)
 
@@ -28,7 +29,7 @@ async function robot() {
             q: query,
             searchType: 'image',
             imgSize: 'huge',
-            num: 2
+            num: 3
         })
 
         const imageUrl = response.data.items.map((item => {
@@ -38,25 +39,30 @@ async function robot() {
         return imageUrl
     }
 
-    async function downloadAllImages(content){
+    async function downloadAllImages(content) {
         content.downloadedImages = []
 
-        for(let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
             const images = content.sentences[sentenceIndex].images
 
-            for(let imageIndex =0; imageIndex < images.length; imageIndex++) {
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
                 const imageUrl = images[imageIndex]
 
                 try {
+
                     if (content.downloadedImages.includes(imageUrl)) {
                         throw new Error('> A imagem já foi baixada')
                     }
 
-                    await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`)
+                    if (imgUrlBlackList.includes(imageUrl)){
+                        throw new Error('> Imagem está na black list')
+                    }
+
+                    await downloadAndSave(imageUrl, `${sentenceIndex}-original.jpg`)
                     content.downloadedImages.push(imageUrl)
                     console.log(`> [${sentenceIndex}][${imageIndex}] Baixou imagem com sucesso: ${imageUrl}`)
                     break
-                }catch(error) {
+                } catch (error) {
                     console.log(`> [${sentenceIndex}][${imageIndex}] Erro ao baixar (${imageUrl}): ${error}`)
                 }
             }
